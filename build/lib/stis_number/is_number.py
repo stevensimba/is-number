3b04543eaf68a21dea06fb73c6731f72e3b08334
stevensimba <stir2017@gmail.com>
2020-04-01 08:13:34 +0300

    updating v-0.0.6

---
+++ build/lib/stis_number/is_number.py
@@ -0,0 +1,13 @@
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
+
+
+def is_number(in_value):
+    print(in_value)
+    try:
+        float(in_value)
+        return True
+    except ValueError:
+        return False
